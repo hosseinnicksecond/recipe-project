e89e4ba19ecac8275793c78f9c226a7c85c02417
hossein nick <hossinnick62@gmail.com>
2020-10-13 16:30:18 +0330

    add dependency and do clean and install

---
+++ src/main/java/home/train/RecipeApplication.java
@@ -0,0 +1,13 @@
+package home.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApplication.class, args);
+	}
+
+}
