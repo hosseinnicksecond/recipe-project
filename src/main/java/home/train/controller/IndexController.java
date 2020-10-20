a5c264a8487821d170ba7efe524bf5dc68cc0c14
hossein nick <hossinnick62@gmail.com>
2020-10-20 13:51:40 +0330

    add Recipe and Note to model and make oneToOne relation

---
+++ src/main/java/home/train/controller/IndexController.java
@@ -0,0 +1,13 @@
+package home.train.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/"})
+    public String getIndex(){
+        return "Index";
+    }
+}
